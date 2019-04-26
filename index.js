c4ae796a8d0cc9b53c7fdbd82fbdcd0c5dba3b76
keoki33 <gwong323@yahoo.com>
2019-04-26 10:28:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 class Formatter {
-  //add static methods here
+  static capitalize(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  }
+
+  static sanitize(s) {
+    return s.replace(/[^A-Za-z0-9-'" "]+/g, '');
+  }
+  static titleize(s) {
+    let wordsArray = s.split(" ")
+    wordsArray = wordsArray.map(g => this.sanitize(g))
+    let newArray = []
+    const matches = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    newArray.push(this.capitalize(wordsArray.shift()))
+
+    wordsArray.forEach((x) => {
+      if (matches.includes(x)) {
+        newArray.push(x)
+      } else {
+        newArray.push(this.capitalize(x))
+      }
+    })
+
+
+    return newArray.join(" ")
+    // console.log(wordsArray)
+    // console.log(newArray)
+    return newArray
+
+  }
+
+
 }
+
+Formatter.titleize("The do!!g ate the cat")
+
+
+// split all words into array
+// upcase first word and put into new array
+// iterate through rest words
+// if word matches list push into array
+// if does not match upcase then push
+//  'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+    console.log(wordsArray)
+    console.log(newArray)
